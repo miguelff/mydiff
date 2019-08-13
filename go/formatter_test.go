68654bd14bee9fc99df70c99758ff65334f808d3
Miguel Fernández <miguelfernandezfernandez@gmail.com>
2019-08-13 17:40:45 +0200

    Small refactor tests

--- go/formatter_test.go
+++ go/formatter_test.go
@@ -35,7 +35,7 @@ func TestNewFormatter_NonExisting(t *testing.T) {
 // SQL output is provided by skeema/tengo, which is also properly
 // tested.
 func TestSQLFormatter_Format(t *testing.T) {
-	sql1 := []string{
+	schema1 := []string{
 		`CREATE TABLE IF NOT EXISTS tasks (
 			id INT AUTO_INCREMENT,
 			title CHAR(255) NOT NULL,
@@ -43,7 +43,7 @@ func TestSQLFormatter_Format(t *testing.T) {
 		)  ENGINE=INNODB;`,
 	}
 
-	sql2 := []string{
+	schema2 := []string{
 		`CREATE TABLE IF NOT EXISTS tasks (
 			id BIGINT AUTO_INCREMENT,
 			title VARCHAR(255) NOT NULL,
@@ -57,14 +57,6 @@ func TestSQLFormatter_Format(t *testing.T) {
 		)  ENGINE=INNODB;`,
 	}
 
-	s1Name, s2Name := TestCluster.LoadSchemas(t, sql1, sql2)
-	from := NewServer1Schema(s1Name)
-	to := NewServer2Schema(s2Name)
-
-	diff := NewDiff(from, to)
-	sqlFmt, _ := NewFormatter("sql")
-	sql := sqlFmt.Format(diff)
-
 	expected := `ALTER TABLE "tasks" MODIFY COLUMN "id" bigint(20) NOT NULL AUTO_INCREMENT, MODIFY COLUMN "title" varchar(255) NOT NULL, ADD COLUMN "owner_id" int(11) DEFAULT NULL;
 CREATE TABLE "owners" (
   "id" int(11) NOT NULL AUTO_INCREMENT,
@@ -73,5 +65,8 @@ CREATE TABLE "owners" (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 `
 	expected = strings.ReplaceAll(expected, "\"", "`")
+	sqlFmt, _ := NewFormatter("sql")
+	sql := RunDiff(t, schema1, schema2, sqlFmt, NoFormatOptions)
+
 	Equal(t, expected, sql)
 }
